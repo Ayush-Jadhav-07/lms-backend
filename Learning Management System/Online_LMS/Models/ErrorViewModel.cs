0d229f0ca074314df1707be38e358adaef4b8e82
Ash-code-07 <aayushjadhav32@gmail.com>
2026-01-29 17:26:08 +0530

    deleted

--- Learning Management System/Online_LMS/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Online_LMS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
